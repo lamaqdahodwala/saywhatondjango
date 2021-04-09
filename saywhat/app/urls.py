b248a5491b40d3b53ad8e0f3ba0c21687eb8f25e
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-04-09 16:37:18 -0400

    nice

--- saywhat/app/urls.py
+++ saywhat/app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import Index, Detail
 urlpatterns = [
     path('', Index.as_view(), name='index'),
-    path('/post/<int:pk>', Detail.as_view(), name='detail')
+    path('post/<int:pk>', Detail.as_view(), name='detail')
 ]
