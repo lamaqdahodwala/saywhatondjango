4f6d2651b699a26b7e66f7f38864ad0333405296
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-04-10 09:49:13 -0400

    lotsa sutuff

--- saywhat/app/urls.py
+++ saywhat/app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import Index, Detail, newpost
+from .views import Index, Detail
 urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('post/<int:pk>', Detail.as_view(), name='detail'),
-    path('newpost', newpost, name='newpost')
 ]
--- saywhat/app/views.py
+++ saywhat/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, UpdateView, CreateView
+from .forms import PostModelForm
 # Create your views here.
 
 class Index(ListView):
@@ -12,8 +13,6 @@ class Detail(DetailView):
     model = Post
     template_name='reading.html'
 
-def newpost(req):
-    if req.method=='POST':
-        ...
-    elif req.method=='GET':
-        ...
+class NewPost(CreateView):
+    model = Post
+    template_name='newpost.html'
