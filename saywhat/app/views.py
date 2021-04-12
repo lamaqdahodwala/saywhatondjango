06078eb7bc9972c3657ace6b88b912958719cbe5
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-04-12 09:58:56 -0400

    login system

--- saywhat/app/views.py
+++ saywhat/app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.handlers.wsgi import WSGIRequest
 from .models import Post
 from django import http
 from django.views.generic import ListView, DetailView, UpdateView, CreateView
@@ -13,7 +14,7 @@ class Detail(DetailView):
     model = Post
     template_name='reading.html'
 
-def newpost(req):
+def newpost(req:WSGIRequest):
     if req.method == "POST":
         form = PostModelForm(req.POST or None)
         print(form.is_valid())
@@ -28,12 +29,17 @@ def newpost(req):
             print('not saved')
             return redirect('index')
     else:
-        return render(req, 'newpost.html')
+        user = req.user 
+        return render(req, 'newpost.html', {'user': user})
 
-def register(req):
+def register(req:WSGIRequest):
     if req.method == 'POST':
-        form = CreateForm()
+        form = CreateForm(req.POST or None)
         if form.is_valid():
             form.save()
+            print('saved form')
+            return redirect('/')
+        else:
+            print('not saved')
     form = CreateForm()
     return render(req, 'register.html', {'form': form})
